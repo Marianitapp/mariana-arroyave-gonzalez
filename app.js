e33b40bd61b81dc7441ef2562765ad5be62ce4cc
Marianitapp <123569229+Marianitapp@users.noreply.github.com>
2023-02-06 12:04:48 -0500

    vars bien

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: 'mcqueen'
+      img:'imagenes/rayo.png'
+     
+    },
+    {
+      name: 'Sterling'
+      img:'imagenes/.inom.jpg'
+     
+    },
+     {
+      name: 'Jackson storm'
+      img:'imagenes/juli.jpg'
+     },
+     {
+      name: 'mcqueen'
+      img:'imaganes/rayo.png'
+     
+    }
+    }
+  ]
+});
